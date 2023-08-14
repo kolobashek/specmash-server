52f54abcf4568851973528b4610075773c14f4b9
kolobashek <kolobashek@gmail.com>
2023-08-14 10:17:38 +0700

    auth done

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,214 +1,213 @@
-import { Model } from 'objection';
+import { Model } from 'objection'
 import { hash, compare } from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { knex } from '../db'
 import Role from './role'
-import logger from '../config/logger';
+import logger from '../config/logger'
 
 const signingKey = process.env.JWT_SECRET || 'secret'
 
 interface User {
-  id: number
-  name: string
-  nickname: string
-  phone: string
-  password: string
-  hash: string
-  roleId: number
-  isActive: boolean
-  comment: string
+	id: number
+	name: string
+	nickname: string
+	phone: string
+	password: string
+	hash: string
+	roleId: number
+	isActive: boolean
+	comment: string
 }
 class User extends Model implements User {
-  constructor(
-    phone?: string,
-    hash?: string,
-    password?: string,
-    id?: number,
-    name?: string,
-    nickname?: string,
-    roleId?: number,
-    isActive?: boolean,
-    comment?: string
-  ) {
-    super()
-    this.id = id || 0
-    this.hash = hash || ''
-    this.password = password || ''
-    this.name = name || ''
-    this.nickname = nickname || ''
-    this.phone = phone || ''
-    this.roleId = roleId || 3
-    this.isActive = isActive || false
-    this.comment = comment || ''
-  }
-  static async create(data: any) {
-    try {
-      const { password, phone, ...userData } = data
-      // Добавление умолчаний
-      userData.roleId = 3
-      userData.isActive = false
-      const isUserExists = await this.findByPhone(phone)
-      if (isUserExists) {
-        return Promise.reject(
-          new Error('Пользователь с таким номером уже есть')
-        )
-      }
-      const hashedPassword = await hash(password, 10)
-      const newUser = await this.query().insert({
-        ...userData,
-        phone,
-        password: hashedPassword,
-      })
-      return newUser
-    } catch (error: any) {
-      logger.error(error)
-      throw new Error(error)
-    }
-  }
-  static async login(phone:any, password:any){
-    
-      const user = await User.getUserByPhone(phone, password)
-      if (user instanceof Error) {
-        return user
-      }
-      const { ...payload } = user
-      // console.log(payload)
-      const token = await (jwt as any).sign(payload, signingKey, {
-        subject: payload.id.toString(),
-        expiresIn: '1d',
-      })
-      return token
-  }
+	constructor(
+		phone?: string,
+		hash?: string,
+		password?: string,
+		id?: number,
+		name?: string,
+		nickname?: string,
+		roleId?: number,
+		isActive?: boolean,
+		comment?: string
+	) {
+		super()
+		this.id = id || 0
+		this.hash = hash || ''
+		this.password = password || ''
+		this.name = name || ''
+		this.nickname = nickname || ''
+		this.phone = phone || ''
+		this.roleId = roleId || 3
+		this.isActive = isActive || false
+		this.comment = comment || ''
+	}
+	static async create(data: any) {
+		try {
+			const { password, phone, ...userData } = data
+			// Добавление умолчаний
+			userData.roleId = 3
+			userData.isActive = false
+			const isUserExists = await this.findByPhone(phone)
+			if (isUserExists) {
+				return Promise.reject(new Error('Пользователь с таким номером уже есть'))
+			}
+			const hashedPassword = await hash(password, 10)
+			const newUser = await this.query().insert({
+				...userData,
+				phone,
+				password: hashedPassword,
+			})
+			return newUser
+		} catch (error: any) {
+			logger.error(error)
+			throw new Error(error)
+		}
+	}
+	static async login(phone: any, password: any) {
+		const user = await User.getUserByPhone(phone, password)
+		if (user instanceof Error) {
+			return user
+		}
+		const { ...payload } = user
+		// console.log(payload)
+		const token = await (jwt as any).sign(payload, signingKey, {
+			subject: payload.id.toString(),
+			expiresIn: '1d',
+		})
+		return token
+	}
+	static async getUserByHash(hash: string) {
+		if (hash) {
+			const payload = jwt.verify(hash, signingKey)
+			if (typeof payload === 'string') {
+				console.log(payload)
+				return null
+			}
+			const user = await User.getUserById(payload.id)
+			return user
+		}
+		return null
+	}
+	async getHashByPhone() {
+		try {
+			const user = await User.findByPhone(this.phone)
+			if (!user || user instanceof Error) {
+				throw new Error('Пользователь не найден')
+			}
+			this.hash = user.hash
+		} catch (error: any) {
+			return new Error(error)
+		}
+	}
 
-  async getHashByPhone() {
-    try {
-      const user = await User.findByPhone(this.phone)
-      if (!user || user instanceof Error) {
-        throw new Error('Пользователь не найден')
-      }
-      this.hash = user.hash
-    } catch (error: any) {
-      return new Error(error)
-    }
-  }
+	async passwordCompare() {
+		try {
+			const user = await User.query().where({ id: this.id }).first()
+			if (!user) {
+				return new Error('Пользователь не найден')
+			}
+			return await compare(this.hash, user.hash)
+		} catch (error: any) {
+			return new Error(error)
+		}
+	}
 
-  async passwordCompare() {
-    try {
-      const user = await User.query().where({ id: this.id }).first()
-      if (!user) {
-        return new Error('Пользователь не найден')
-      }
-      return await compare(this.hash, user.hash)
-    } catch (error: any) {
-      return new Error(error)
-    }
-  }
+	static async isActive(userId: Number): Promise<boolean> {
+		const { isActive } = await knex.select('isActive').from('users').where('id', userId).first()
+		return isActive
+	}
 
-  static async isActive(userId: Number): Promise<boolean> {
-    const { isActive } = await knex
-      .select('isActive')
-      .from('users')
-      .where('id', userId)
-      .first()
-    return isActive
-  }
+	static async getUserByPhone(phone: string, password: string): Promise<User | Error> {
+		try {
+			const user = await User.query().withGraphFetched('role').where({ phone }).first()
+			if (!user) {
+				return new Error('Пользователь не найден')
+			}
+			const isValid = await compare(password, user.password)
+			if (!isValid) {
+				return new Error('Неверный пароль')
+			}
+			return user
+		} catch (error: any) {
+			return error
+		}
+	}
 
-  static async getUserByPhone(
-    phone: string,
-    password: string
-  ): Promise<User | Error> {
-    try {
-      const user = await User.query().withGraphFetched('role').where({ phone }).first()
-      if (!user) {
-        return new Error('Пользователь не найден')
-      }
-      const isValid = await compare(password, user.password)
-      if (!isValid) {
-        return new Error('Неверный пароль')
-      }
-      return user
-    } catch (error: any) {
-      return error
-    }
-  }
+	static async getUserById(id: Number) {
+		try {
+			const user = await User.query().withGraphFetched('role').where({ id }).first()
+			if (!user) {
+				return new Error('Пользователь не найден')
+			}
+			return user
+		} catch (error: any) {
+			return new Error(error)
+		}
+	}
 
-  static async getUserById(id: Number) {
-    try {
-      const user = await User.query().where({ id }).first()
-      if (!user) {
-        return new Error('Пользователь не найден')
-      }
-      return user
-    } catch (error: any) {
-      return new Error(error)
-    }
-  }
+	static async findByPhone(phone: string) {
+		try {
+			const user = await this.query().where({ phone }).first()
+			if (!user) {
+				return null
+			}
+			return user
+		} catch (error: any) {
+			return new Error(error)
+		}
+	}
 
-  static async findByPhone(phone: string) {
-    try {
-      const user = await this.query().where({ phone }).first()
-      if (!user) {
-        return null
-      }
-      return user
-    } catch (error: any) {
-      return new Error(error)
-    }
-  }
+	static async activateUser(id: Number) {
+		const isActive = await this.isActive(id)
+		// console.log(`[model]: - ${isActive},${id}`)
+		const result = await knex('users').where('id', id).update({ isActive: !isActive })
+		return { isActive: !isActive }
+	}
 
-  static async activateUser(id: Number) {
-    const isActive = await this.isActive(id)
-    // console.log(`[model]: - ${isActive},${id}`)
-    const result = await knex('users')
-      .where('id', id)
-      .update({ isActive: !isActive })
-    return { isActive: !isActive }
-  }
+	static get tableName() {
+		return 'users'
+	}
 
-  static get tableName() {
-    return 'users'
-  }
+	static get idColumn() {
+		return 'id'
+	}
 
-  static get idColumn() {
-    return 'id'
-  }
+	static get jsonSchema() {
+		return {
+			type: 'object',
+			required: ['name', 'phone', 'password', 'role'],
 
-  static get jsonSchema() {
-    return {
-      type: 'object',
-      required: ['name', 'phone', 'password', 'role'],
+			properties: {
+				id: { type: 'integer' },
+				name: { type: 'string', minLength: 1, maxLength: 255 },
+				nickname: { type: 'string', maxLength: 255 },
+				phone: { type: 'string', minLength: 3, maxLength: 25 },
+				role: {
+					type: 'string',
+					enum: ['admin', 'manager', 'driver'],
+					default: 'driver',
+				},
+				password: { type: 'string', minLength: 1, maxLength: 255 },
+				isActive: { type: 'boolean' },
+				comment: { type: 'string', maxLength: 255 },
+			},
+		}
+	}
 
-      properties: {
-        id: { type: 'integer' },
-        name: { type: 'string', minLength: 1, maxLength: 255 },
-        nickname: { type: 'string', maxLength: 255 },
-        phone: { type: 'string', minLength: 3, maxLength: 25 },
-        role: {
-          type: 'string',
-          enum: ['admin', 'manager', 'driver'],
-          default: 'driver',
-        },
-        password: { type: 'string', minLength: 1, maxLength: 255 },
-        isActive: { type: 'boolean' },
-        comment: { type: 'string', maxLength: 255 },
-      },
-    }
-  }
-
-  static get relationMappings() {
-    // const users = await User.query()
-    //   .withGraphFetched('role')
-    //   .where({ isActive: true });
-    return {
-      role: {
-        relation: Model.BelongsToOneRelation,
-        modelClass: Role,
-        join: {
-          from: 'users.roleId',
-          to: 'roles.id',
-        },
-      },
-    }
-  }
+	static get relationMappings() {
+		// const users = await User.query()
+		//   .withGraphFetched('role')
+		//   .where({ isActive: true });
+		return {
+			role: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: Role,
+				join: {
+					from: 'users.roleId',
+					to: 'roles.id',
+				},
+			},
+		}
+	}
 }
 export default User
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,112 +1,116 @@
-import logger from './config/logger';
-import Role from './models/role';
+import logger from './config/logger'
+import Role from './models/role'
 import User from './models/user'
 import { GraphQLError } from 'graphql'
 
 const resolvers = {
-  Query: {
-    users: async () => {
-      // получить пользователей из БД
-      const users = await User.query()
-      return users
-    },
+	Query: {
+		users: async () => {
+			// получить пользователей из БД
+			const users = await User.query()
+			return users
+		},
 
-    roles: async () => {
-      // получить роли из БД
-      const roles = await Role.query()
-      return roles
-    },
+		roles: async () => {
+			// получить роли из БД
+			const roles = await Role.query()
+			return roles
+		},
 
-    equipment: async () => {
-      // получить оборудование из БД
-    },
+		equipment: async () => {
+			// получить оборудование из БД
+		},
 
-    objects: async () => {
-      // получить объекты из БД
-    },
+		objects: async () => {
+			// получить объекты из БД
+		},
 
-    travelLogs: async () => {
-      // получить путевые листы из БД
-    },
+		travelLogs: async () => {
+			// получить путевые листы из БД
+		},
 
-    findByPhone: async (_: any, { phone }: { phone: string }) => {
-      const user = await User.query().where({ phone }).first()
-      return user
-    },
-    isActive: async (
-      root: any,
-      { userId }: { userId: Number },
-      context: any
-    ): Promise<boolean> => {
-      // Получаем пользователя по id
-      const result = await User.isActive(userId)
-      logger.debug(JSON.stringify(result))
-      // Возвращаем флаг активности
-      return result
-    },
-    me: (parent: any, args: any, ctx: any) => {
-      // The content of the JWT can be found in the context
-      const headers = ctx.request.headers.headersInit.authorization
-      console.log(headers)
-      if (!ctx.jwt) {
-        // No JWT token provided, we are not authenticated
-        return null
-      }
-      return User.getUserById(ctx.jwt.sub)
-    },
-  },
-  Mutation: {
-    register: async (_: any, { input }: { input: CreateUserInput }) => {
-      try {
-        const user = await User.create(input)
-        return user
-      } catch (error: any) {
-        return new GraphQLError(error.message)
-      }
-    },
-    activateUser: async (parent: any, { input }: UserIdInput) => {
-      try {
-        const { userId } = input
-        // Активация пользователя по id
-        const result = await User.activateUser(userId)
-        return result
-      } catch (error: any) {
-        return new GraphQLError(error.message)
-      }
-    },
-    login: async (parent: any, { phone, password }: any, ctx: any) => {
-      const token = await User.login(phone, password)
-      if (token instanceof Error) {
-        return new GraphQLError('Invalid credentials')
-      }
-      // Set the cookie on the response
-      ctx.request.cookieStore?.set({
-        name: 'authorization',
-        sameSite: 'strict',
-        secure: true,
-        domain: 'specmash.su',
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // 24 hours
-        value: token,
-        httpOnly: true,
-      })
-      return { token }
-    },
-  },
+		findByPhone: async (_: any, { phone }: { phone: string }) => {
+			const user = await User.query().where({ phone }).first()
+			return user
+		},
+		isActive: async (root: any, { userId }: { userId: Number }, context: any): Promise<boolean> => {
+			// Получаем пользователя по id
+			const result = await User.isActive(userId)
+			logger.debug(JSON.stringify(result))
+			// Возвращаем флаг активности
+			return result
+		},
+		me: async (parent: any, args: any, ctx: any) => {
+			// The content of the JWT can be found in the context
+			const token: string = ctx.request.headers.headersInit.authorization
+			// console.log(token)
+			if (token) {
+				const user = await User.getUserByHash(token)
+				if (typeof user === 'string') {
+					// console.log(user)
+					return new GraphQLError(user)
+				} else if (!user) {
+					return new GraphQLError('Неверный токен')
+				}
+				// console.log(user)
+				return user
+			}
+			console.log(token)
+			return new GraphQLError('Пользователь не авторизован')
+		},
+	},
+	Mutation: {
+		register: async (_: any, { input }: { input: CreateUserInput }) => {
+			try {
+				const user = await User.create(input)
+				return user
+			} catch (error: any) {
+				return new GraphQLError(error.message)
+			}
+		},
+		activateUser: async (parent: any, { input }: UserIdInput) => {
+			try {
+				const { userId } = input
+				// Активация пользователя по id
+				const result = await User.activateUser(userId)
+				return result
+			} catch (error: any) {
+				return new GraphQLError(error.message)
+			}
+		},
+		login: async (parent: any, { phone, password }: any, ctx: any) => {
+			const token = await User.login(phone, password)
+			if (token instanceof Error) {
+				return new GraphQLError('Invalid credentials')
+			}
+			// Set the cookie on the response
+			ctx.request.cookieStore?.set({
+				name: 'authorization',
+				sameSite: 'strict',
+				secure: true,
+				domain: 'specmash.su',
+				expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // 24 hours
+				value: token,
+				httpOnly: true,
+			})
+			return { token }
+		},
+	},
 }
 
 export default resolvers
 
 type CreateUserInput = {
-  name: string
-  password: string
-  phone: string
-  roleId?: number
-  isActive?: boolean
-  nickname?: string
-  comment?: string
+	name: string
+	password: string
+	phone: string
+	roleId?: number
+	isActive?: boolean
+	nickname?: string
+	comment?: string
 }
 type UserIdInput = {
-  input:{
-    userId: number
-  }
+	input: {
+		userId: number
+	}
 }
